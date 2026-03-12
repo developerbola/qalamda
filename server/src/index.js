d2bbe027c8472717410a71a9907b2d07deea9f7f
developerbola <developerbola08@gmail.com>
2026-03-12 11:24:30 +0500

    fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,18 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
 import { prettyJSON } from "hono/pretty-json";
 
-// Import controllers
-import * as authController from "./controllers/authController";
-import * as userController from "./controllers/userController";
-import * as articleController from "./controllers/articleController";
-import * as commentController from "./controllers/commentController";
-import * as likeController from "./controllers/likeController";
-import * as bookmarkController from "./controllers/bookmarkController";
-import * as tagController from "./controllers/tagController";
-
-// Import middleware
-import * as middleware from "./middlewares/auth.middleware";
-
 // Import routes
-import { healthCheckRoute, authRoutes, userRoutes, articleRoutes, commentRoutes, likeRoutes, tagRoutes } from "./routes/apiRoutes";
+import {
+  healthCheckRoute,
+  authRoutes,
+  userRoutes,
+  articleRoutes,
+  commentRoutes,
+  likeRoutes,
+  tagRoutes,
+} from "./routes/apiRoutes";
 
 const app = new Hono();
 
@@ -23,28 +20,25 @@ const app = new Hono();
 app.use("*", cors());
 app.use("*", prettyJSON());
 
-// Health check
 healthCheckRoute(app);
-
-// Auth routes
 authRoutes(app);
-
-// User routes
 userRoutes(app);
-
-// Article routes
 articleRoutes(app);
-
-// Comment routes
 commentRoutes(app);
-
-// Like routes
 likeRoutes(app);
-
-// Tag routes
 tagRoutes(app);
 
-export default {
-  port: 3001,
-  fetch: app.fetch,
-};
+
+// export default {
+//   port: 3001,
+//   fetch: app.fetch,
+// };
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
+export const DELETE = handler;
